531c61bece987d1c4a979109b1844cac684de982
jung bo <pej0900@naver.com>
2024-09-13 04:41:02 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+# my_module_with_tests.py
+
+# 테스트할 함수들
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+# 테스트 코드
+import unittest
+
+class TestMyModule(unittest.TestCase):
+    def test_functions(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(multiply(2, 3), 6)
+
+if __name__ == '__main__':
+    unittest.main()
